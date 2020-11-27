599cd3c0206e6474b5d5d7d11af64ede2280d39d
baskle <baaklee@gmail.com>
2020-11-27 20:17:29 +0100

    added search bar and API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const APP_ID = "e7de1eaf";
+  const APP_KEY = "0c66e30803206c41ffd23177884443f9";
+
+  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   return (
-    <div>
-      <h1>Hello und moin</h1>
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text"></input>
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
