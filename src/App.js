6cbacc98e5b1af59ae1a708a7bfbfee66c87d732
baskle <baaklee@gmail.com>
2020-12-02 21:21:08 +0100

    new test

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Recipe from "./Recipe";
+const { KeyVault } = require("./secrets.json");
+
+const App = () => {
+  // const APP_ID = "e7de1eaf";
+  // const APP_KEY = "0c66e30803206c41ffd23177884443f9";
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${KeyVault.APP_ID}&app_key=${KeyVault.APP_KEY}`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log("I chnage the code, and i liked it");
+  };
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+    console.log(search);
+  };
+
+  return (
+    <div className="App">
+      <form className="search-form">
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        ></input>
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      {recipes.map((recipe) => (
+        <Recipe
+          key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default App;
