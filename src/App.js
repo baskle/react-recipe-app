175c8db8d79c2db9825140375f1400f34371ec71
Imad Arain <arain.imad@gmail.com>
2020-12-02 20:41:16 +0100

    gitignore updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import Recipe from "./Recipe";
+const {KeyVault} = require ("./secrets.json")
 
 const App = () => {
-  const APP_ID = "e7de1eaf";
-  const APP_KEY = "0c66e30803206c41ffd23177884443f9";
+  // const APP_ID = "e7de1eaf";
+  // const APP_KEY = "0c66e30803206c41ffd23177884443f9";
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
@@ -15,7 +16,7 @@ const App = () => {
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?q=chicken&app_id=${KeyVault.APP_ID}&app_key=${KeyVault.APP_KEY}`
     );
     const data = await response.json();
     setRecipes(data.hits);
