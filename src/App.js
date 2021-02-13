84f4fd86f2c1e16e2a2df17381a62286ad886871
baskle <baaklee@gmail.com>
2021-02-13 21:25:36 +0100

    added nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import Recipe from "./Recipe";
+import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+
 const { KeyVault } = require("./secrets.json");
 
 const App = () => {
@@ -17,7 +21,7 @@ const App = () => {
     );
     const data = await response.json();
     setRecipes(data.hits);
-    console.log("I chnage the code, and i liked it");
+    console.log("changed");
   };
 
   const updateSearch = (e) => {
@@ -26,27 +30,44 @@ const App = () => {
   };
 
   return (
-    <div className="App">
-      <form className="search-form">
-        <input
-          className="search-bar"
-          type="text"
-          value={search}
-          onChange={updateSearch}
-        ></input>
-        <button className="search-button" type="submit">
-          Suche
-        </button>
-      </form>
-      {recipes.map((recipe) => (
-        <Recipe
-          key={recipe.recipe.label}
-          title={recipe.recipe.label}
-          calories={recipe.recipe.calories}
-          image={recipe.recipe.image}
-        />
-      ))}
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
+
+      <div className="App">
+        <form className="search-form">
+          <input
+            className="search-bar"
+            type="text"
+            value={search}
+            onChange={updateSearch}
+          ></input>
+          <button className="search-button" type="submit">
+            Suche
+          </button>
+        </form>
+        {recipes.map((recipe) => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+          />
+        ))}
+      </div>
+    </Router>
   );
 };
 
